1dfa41006b55ddb1a12813aa89788acd64d39be5
Riley Evans <rllyy97@gmail.com>
2019-04-24 21:04:14 -0500

    Initial commit

---
+++ Assets/_Completed-Game/Scripts/PlayerController.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+
+// Include the namespace required to use Unity UI
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	// Create public variables for player speed, and for the Text UI game objects
+	public float speed;
+	public Text countText;
+	public Text winText;
+    public Slider speedBar;
+
+	// Create private references to the rigidbody component on the player, and the count of pick up objects picked up so far
+	private Rigidbody rb;
+    private Collider coll;
+	private int count;
+
+    // Jump Variables
+    private Vector3 jump;
+    public bool jumpLate;
+    public float jumpScale = 2.0f;
+    public float gravity = 2.0f;
+    private float groundDistance;
+
+
+    // Camera Control
+    public Camera cam;
+    public OrbitCameraController camController;
+    public string rotateCameraXInput = "Mouse X";
+    public string rotateCameraYInput = "Mouse Y";
+
+    // At the start of the game..
+    void Start ()
+	{
+		// Assign the Rigidbody component to our private rb variable
+		rb = GetComponent<Rigidbody>();
+        coll = GetComponent<Collider>();
+
+		// Set the count to zero 
+		count = 0;
+
+		// Run the SetCountText function to update the UI (see below)
+		SetCountText ();
+
+		// Set the text property of our Win Text UI to an empty string, making the 'You Win' (game over message) blank
+		winText.text = "";
+
+        // Init jump variable
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+        groundDistance = coll.bounds.extents.y;
+
+        // Handle Cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, groundDistance + 0.2f);
+    }
+
+    bool IsAlmostGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, groundDistance + 2.0f);
+    }
+
+    // Each physics step..
+    void FixedUpdate ()
+	{
+		// Set some local float variables equal to the value of our Horizontal and Vertical Inputs
+		float horizontalAxis = Input.GetAxis ("Horizontal");
+		float verticalAxis = Input.GetAxis ("Vertical");
+
+        // Setup Camera
+        CameraInput();
+
+        // Get directions relative to camera
+        var forward = cam.transform.forward;
+        var right = cam.transform.right;
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+        var desiredMoveDirection = forward * verticalAxis + right * horizontalAxis;
+
+
+        // Create a Vector3 variable, and assign X and Z to feature our horizontal and vertical float variables above
+        Vector3 movement = new Vector3 (desiredMoveDirection.x, 0.0f, desiredMoveDirection.z);
+
+		// Add a physical force to our Player rigidbody using our 'movement' Vector3 above, 
+		// multiplying it by 'speed' - our public player speed that appears in the inspector
+		rb.AddForce (movement * speed);
+
+        // Gravity
+        rb.AddForce(Vector3.down * gravity * rb.mass);
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.AddForce(jump * jumpScale, ForceMode.Impulse);
+        }
+
+        speedBar.value = rb.velocity.magnitude;
+    }
+
+    // Control Camera
+    protected virtual void CameraInput()
+    {
+        var Y = Input.GetAxis(rotateCameraYInput);
+        var X = Input.GetAxis(rotateCameraXInput);
+        camController.RotateCamera(X, Y);
+    }
+
+    // Control Collisions
+    void OnTriggerEnter(Collider other) 
+	{
+        // Control Pick Up Event
+   		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			// Make the other game object (the pick up) inactive, to make it disappear
+			other.gameObject.SetActive (false);
+
+			// Add one to the score variable 'count'
+			count = count + 1;
+
+			// Run the 'SetCountText()' function
+			SetCountText ();
+		}
+	}
+
+	// Create a standalone function that can update the 'countText' UI and check if the required amount to win has been achieved
+	void SetCountText()
+	{
+		// Update the text field of our 'countText' variable
+		countText.text = count.ToString ();
+
+		// Check if our 'count' is equal to or exceeded 12
+		if (count >= 12) 
+		{
+			// Set the text value of our 'winText'
+			winText.text = "Win Text";
+		}
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# CS-4610-Final-Project
