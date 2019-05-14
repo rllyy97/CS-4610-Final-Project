424acd4c59ef379d45c65c6ab43762bc5e9bd287
Riley Evans <rllyy97@gmail.com>
2019-05-14 01:08:30 -0500

    Added online leaderboard
    Displaying still needs work but time submission is functional

--- Assets/_Completed-Game/Scripts/PlayerController.cs
+++ Assets/_Completed-Game/Scripts/PlayerController.cs
@@ -3,19 +3,28 @@
 // Include the namespace required to use Unity UI
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
 
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour {
 
     // UI Variables
-	public Text countText;
+	private Text countText;
     private int count;
-    public Text winText;
-    public Text timeText;
+    private Text winText;
+    private Text timeText;
     private float time;
-    public Slider speedBar;
-    public Image windowShade;
+    private Slider speedBar;
+    private Image windowShade;
+    private InputField playerNameInput;
+    private Button submitScoreButton;
+    private Text leaderboardNames;
+    private Text leaderboardTimes;
+    private Text leaderboardNumbers;
 
     // Sound Variables
     public AudioSource bounceSource;
@@ -65,9 +74,19 @@ public class PlayerController : MonoBehaviour {
 
     // Pause + Menu variables
     public bool paused = false;
+    public bool won = false;
     private bool timerRunning = true;
     private Vector3 savedVelocity;
     private Vector3 savedAngularVelocity;
+
+    // Database Variables
+    public Dictionary<string, float> leaderBoard = new Dictionary<string, float>();
+    private const int MaxScores = 10;
+    private string playerName = "testPlayer";
+    private float finishTime = 100;
+    const int kMaxLogSize = 16382;
+    DependencyStatus dependencyStatus = DependencyStatus.UnavailableOther;
+    protected bool isFirebaseInitialized = false;
 
     // Pickup Variables
     private int pickupMax;
@@ -81,10 +100,27 @@ public class PlayerController : MonoBehaviour {
         pickupMax = GameObject.FindGameObjectsWithTag("Pick Up").Length;
 
         // Init UI
+        countText = GameObject.Find("Pickup Count Text").GetComponent<Text>();
+        timeText = GameObject.Find("Time Text").GetComponent<Text>();
+        winText = GameObject.Find("Win Text").GetComponent<Text>();
+        speedBar = GameObject.Find("Speed Bar").GetComponent<Slider>();
+        windowShade = GameObject.Find("Window Shade").GetComponent<Image>();
+        playerNameInput = GameObject.Find("Player Name Input").GetComponent<InputField>();
+        submitScoreButton = GameObject.Find("Submit Score Button").GetComponent<Button>();
+        leaderboardNames = GameObject.Find("Leaderboard Names").GetComponent<Text>();
+        leaderboardTimes = GameObject.Find("Leaderboard Times").GetComponent<Text>();
+        leaderboardNumbers = GameObject.Find("Leaderboard Numbers").GetComponent<Text>();
+
         count = 0;
         countText.text = "0 / " + pickupMax;
-		winText.text = "";
+        winText.text = "";
         windowShade.enabled = false;
+        leaderboardNames.enabled = false;
+        leaderboardTimes.enabled = false;
+        leaderboardNumbers.enabled = false;
+        playerNameInput.gameObject.SetActive(false);
+        submitScoreButton.gameObject.SetActive(false);
+
 
         // Init jump variable
         jump = new Vector3(0.0f, 2.0f, 0.0f);
@@ -98,6 +134,90 @@ public class PlayerController : MonoBehaviour {
         musicSource.loop = true;
         musicSource.volume = musicVolume;
         musicSource.Play();
+
+        // Firebase 
+        leaderBoard.Clear();
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available) InitializeFirebase();
+            else Debug.LogError("Could not resolve all Firebase dependencies: " + dependencyStatus);
+        });
+        
+
+    }
+
+    // Initialize Firebase (duh)
+    protected virtual void InitializeFirebase()
+    {
+        FirebaseApp app = FirebaseApp.DefaultInstance;
+        app.SetEditorDatabaseUrl("https://smemrmg2sf.firebaseio.com/");
+        if (app.Options.DatabaseUrl != null) app.SetEditorDatabaseUrl(app.Options.DatabaseUrl);
+        StartListener();
+        isFirebaseInitialized = true;
+
+        FirebaseDatabase.DefaultInstance.GetReference("leaderboard")
+            .Child(SceneManager.GetActiveScene().name).OrderByChild("time").GetValueAsync().ContinueWith(task =>
+            {
+                if (task.IsCompleted)
+                {
+                    DataSnapshot snapshot = task.Result;
+                    foreach (var child in snapshot.Children)
+                        leaderBoard.Add(child.Child("playerName").Value.ToString(), (float) child.Child("time").Value);
+                };
+            });
+        
+    }
+
+    protected void StartListener()
+    {
+        FirebaseDatabase.DefaultInstance.GetReference("leaderboard").Child(SceneManager.GetActiveScene().name).OrderByChild("time")
+          .ValueChanged += (object sender2, ValueChangedEventArgs e2) => {
+              if (e2.DatabaseError != null) { Debug.LogError(e2.DatabaseError.Message); return; }
+              leaderBoard.Clear();
+              if (e2.Snapshot != null && e2.Snapshot.ChildrenCount > 0)
+                  foreach (var child in e2.Snapshot.Children)
+                      leaderBoard.Add(child.Child("playerName").Value.ToString(), (float)child.Child("time").Value);
+          };
+    }
+
+    TransactionResult AddScoreTransaction(MutableData mutableData)
+    {
+        List<object> leaders = mutableData.Value as List<object>;
+        if (leaders == null) leaders = new List<object>();
+        else if (mutableData.ChildrenCount >= MaxScores)
+        {
+            long maxTime = 0;
+            object maxVal = null;
+            foreach (var child in leaders)
+            {
+                if (!(child is Dictionary<string, object>)) continue;
+                long childTime = (long)((Dictionary<string, object>)child)["time"];
+                if (childTime > maxTime)
+                {
+                    maxTime = childTime;
+                    maxVal = child;
+                }
+            }
+            // Not fast enough
+            if (maxTime < finishTime) return TransactionResult.Abort();
+            // Kick lowest score
+            leaders.Remove(maxVal);
+        }
+
+        // Insert new player record time
+        Dictionary<string, object> newScoreMap = new Dictionary<string, object>();
+        newScoreMap["time"] = finishTime;
+        newScoreMap["playerName"] = playerName;
+        leaders.Add(newScoreMap);
+
+        mutableData.Value = leaders;
+        return TransactionResult.Success(mutableData);
+    }
+
+    public void AddScore()
+    {
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.GetReference("leaderboard").Child(SceneManager.GetActiveScene().name);
+        reference.RunTransaction(AddScoreTransaction);
 
     }
 
@@ -121,38 +241,31 @@ public class PlayerController : MonoBehaviour {
     // Run consistently (user input)
     void Update()
     {
-        // ESCAPE
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (!won)
         {
-            if (paused) Unpause();
-            else Pause();
-        }
+            // ESCAPE
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (paused) Unpause();
+                else Pause();
+            }
 
-        // SPACE
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (IsGrounded()) ForceJump();
-            else if (IsAlmostGrounded() && rb.velocity.normalized.y < 0) jumpLate = true;
-        }
+            // SPACE
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (IsGrounded()) ForceJump();
+                else if (IsAlmostGrounded() && rb.velocity.normalized.y < 0) jumpLate = true;
+            }
 
-        // R
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
+            // R
+            if (Input.GetKeyDown(KeyCode.R)) SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 
-        // Backspace
-        if (Input.GetKeyDown(KeyCode.Backspace))
-        {
-            OutOfBounds();
-        }
+            // Backspace
+            if (Input.GetKeyDown(KeyCode.Backspace)) OutOfBounds();
 
-        // Enter
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            Win();
+            // Enter
+            if (Input.GetKeyDown(KeyCode.Return)) Win();
         }
-
     }
 
     // Run once per frame (avoid user input here)
@@ -306,6 +419,7 @@ public class PlayerController : MonoBehaviour {
         alertSource.PlayOneShot(uiDown);
         musicSource.volume = musicVolume / 2;
 
+        showLeaderboard();
     }
 
     void Unpause()
@@ -326,13 +440,59 @@ public class PlayerController : MonoBehaviour {
         // Sound
         alertSource.PlayOneShot(uiUp);
         musicSource.volume = musicVolume;
+
+        hideLeaderboard();
     }
 
     void Win()
     {
         timerRunning = false;
-        winText.text = "Win Text";
+        paused = true;
+        won = true;
+
+        // Freeze
+        savedVelocity = rb.velocity;
+        savedAngularVelocity = rb.angularVelocity;
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+
+        // UI Changes
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        windowShade.enabled = true;
+        winText.text = "Stage Complete!\nTime: " + timeText.text;
+        speedBar.gameObject.SetActive(false);
+        countText.enabled = false;
+        timeText.enabled = false;
+
+        // Sound
+        rolling = false;
+        rollSource.volume = 0;
+        rollSource.Stop();
         alertSource.PlayOneShot(winSound);
+        musicSource.volume = musicVolume / 2;
+
+        finishTime = time;
+        playerNameInput.gameObject.SetActive(true);
+        playerNameInput.Select();
+        playerNameInput.ActivateInputField();
+        submitScoreButton.gameObject.SetActive(true);
+        submitScoreButton.interactable = true;
+        submitScoreButton.onClick.AddListener(delegate {
+            if (playerNameInput.text != "")
+            {
+                playerName = playerNameInput.text;
+                AddScore();
+                playerNameInput.gameObject.SetActive(false);
+                submitScoreButton.gameObject.SetActive(false);
+                winText.enabled = false;
+
+                leaderBoard.Add(playerName, finishTime);
+
+                showLeaderboard();
+            }
+        });
+
+        
 
         /*
         // Blink Time
@@ -346,6 +506,36 @@ public class PlayerController : MonoBehaviour {
         }
         */
 
+    }
+
+    void showLeaderboard()
+    {
+
+        // alertSource.PlayOneShot(winSound);
+
+        // Activate Leaderboard UI
+        leaderboardNames.enabled = true;
+        leaderboardTimes.enabled = true;
+        leaderboardNumbers.enabled = true;
+
+        var count = 1;
+        // Add Numbers, Names, Times
+        foreach (var child in leaderBoard)
+        {
+            leaderboardNames.text = leaderboardNames.text + "\n" + child.Key.ToString();
+            leaderboardTimes.text = leaderboardTimes.text + "\n" + child.Value.ToString();
+            leaderboardNumbers.text = leaderboardNumbers.text + "\n" + count + ".";
+            count++;
+        }
+
+    }
+
+    void hideLeaderboard()
+    {
+        // Hide Leaderboard UI
+        leaderboardNames.enabled = false;
+        leaderboardTimes.enabled = false;
+        leaderboardNumbers.enabled = false;
     }
 
 }
