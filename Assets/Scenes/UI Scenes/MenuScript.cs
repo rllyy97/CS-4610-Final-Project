9dbd0f26fd4637f1ef5bb3a99031d0f4adefe153
Jasmine Tan <jtbpb@mail.missouri.edu>
2019-05-14 20:16:05 -0500

    the ui

---
+++ Assets/Scenes/UI Scenes/MenuScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OpenLevel()
+    {
+        SceneManager.LoadScene("Levels");
+
+    }
+    public void OpenMainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+
+    }
+    public void OpenAbout()
+    {
+        SceneManager.LoadScene("About");
+
+    }
+    public void OpenHelp()
+    {
+        SceneManager.LoadScene("Help");
+
+    }
+    public void OpenPirates()
+    {
+        SceneManager.LoadScene("Pirates");
+
+    }
+    public void OpenSkaterz()
+    {
+        SceneManager.LoadScene("skaterz");
+
+    }
+    public void OpenNature()
+    {
+        SceneManager.LoadScene("Nature");
+
+    }
+    public void OpenSonic()
+    {
+        SceneManager.LoadScene("Sonic");
+
+    }
+    public void OpenNightmare()
+    {
+        SceneManager.LoadScene("Nightmare");
+
+    }
+    public void OpenTutorial()
+    {
+        SceneManager.LoadScene("tutorial");
+
+    }
+    public void OpenHouse()
+    {
+        SceneManager.LoadScene("House");
+
+    }
+    public void OpenSky()
+    {
+        SceneManager.LoadScene("sky");
+
+    }
+}
